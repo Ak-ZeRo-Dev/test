7f861d0485d9bdc0f1b5af06202de94aeb1903d4
Abdulrahman Alkurdi <113478579+Ak-ZeRo-Dev@users.noreply.github.com>
2024-09-06 22:57:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const get = async () => {
-  const res = await fetch(
-    "http://localhost:3050/api/leetcode?username=akzero-dev"
-  );
-  return await res.json();
-};
-
-const data = get()
-console.log(data)
-
-document.querySelector(".test").innerHTML = JSON.stringify(data);
+document.addEventListener('DOMContentLoaded', () => {
+  const get = async () => {
+    const res = await fetch(
+      "https://ak-zero-server-2oyk1kyv8-akzeros-projects.vercel.app/api/leetcode?username=akzero-dev"
+    )
+    const data = await res.json();
+    return data
+  };
+  
+  
+  document.querySelector(".test").innerHTML = JSON.stringify(get());
+  
+})
